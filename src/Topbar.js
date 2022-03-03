8593d0f768eecf0fa622525a4239825d91a86d13
suhye0n <conact.suhyeon@gmail.com>
2022-03-03 23:26:00 +0900

    220303 상단바 및 신호등 디자인 수정

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,13 +1,24 @@
 import './Topbar.css';
+import logo from './logo.png';
  
 function Topbar() {
     return(
         <div>
             <div class="header">
-                <img src="../images/topbar.png"></img>
+                <img class="logo" src={logo}></img>
                 <a href="/">
-                    <h2>TAXIPOOL</h2>
+                    <h2 class="title">TAXIPOOL</h2>
                 </a>
+                <div class="line-wrap">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                </div>
             </div>
         </div>
     )
--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -3,15 +3,15 @@ import './TrafficLight.css';
 function TrafficLight() {
     return(
         <div class="traffic-light">
-            <div class="top">
-                <a href="/login"><button class="login">로그인</button></a>
+            <div class="traffic-top">
+                <a href="/login"><button class="top">로그인</button></a>
                 <button class="mid"></button>
-                <a href="/signup"><button class="signup">회원가입</button></a>
+                <a href="/signup"><button class="bottom">회원가입</button></a>
             </div>
 
-            <div class="middle"></div>
+            <div class="traffic-middle"></div>
 
-            <div class="bottom"></div>
+            <div class="traffic-bottom"></div>
         </div>
     )
 }
