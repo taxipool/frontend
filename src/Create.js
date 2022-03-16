2818e1b333888b778f88fe507c75fd916fb8eca9
yelim1215 <yelim1215jg@naver.com>
2022-03-16 16:52:13 +0900

    ㅇㅇ

--- src/Create.js
+++ src/Create.js
@@ -4,13 +4,55 @@ import './Create.css';
 
 // create 버튼 클릭 이벤트
 const onClickCreate = () => {
-    let obj = {
-        roomname: "myroom",
-        startpoint: "start",
-        endpoint: "end",
+    const [inputs, setData ] = useState({
+        roomname: "",
+        startpoint: "",
+        endpoint: "",
         starttime: new Date(),
-        totalmember: 3,
-        currentmember: 1
+        totalmember: '',
+        currentmember: ''
+    })
+
+    const { roomname, startpoint, endpoint, starttime, totalmember, currentmember} = inputs;
+
+    const onRoomnameChange = (e) => {
+        const { vlaue, roomname } = e.target;
+        setData({
+            ...inputs,
+            [roomname] : value
+        });
+    };
+
+    const onStartpointChange = (e) => {
+        const { vlaue, startpoint } = e.target;
+        setData({
+            ...inputs,
+            [startpoint] : value
+        });
+    };
+
+    const onEndpointChange = (e) => {
+        const { vlaue, endpoint } = e.target;
+        setData({
+            ...inputs,
+            [endpoint] : value
+        });
+    };
+
+    const onStarttimeChange = (e) => {
+        const { vlaue, starttime } = e.target;
+        setData({
+            ...inputs,
+            [starttime] : value
+        });
+    };
+
+    const onTotalmemberChange = (e) => {
+        const { vlaue, totalmember } = e.target;
+        setData({
+            ...inputs,
+            [totalmember] : value
+        });
     };
 
     let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im15aWQiLCJpYXQiOjE1MTYyMzkwMjJ9.SrLa4xS_VbNwYF4Zatu7ilRXCKrOlccvkBPHYV5yJSc"
@@ -19,7 +61,7 @@ const onClickCreate = () => {
         headers: { Authorization: `Bearer ${accessToken}` }
      };
     
-    axios.post('http://taxipool.iptime.org:8080/api/rooms', obj, config)
+    axios.post('http://taxipool.iptime.org:8080/api/rooms', value, config)
     .then(res => {
             console.log(res);
             if (res.status == 200)
@@ -38,14 +80,6 @@ const onClickCreate = () => {
 }
 
 function Create() {
-	// 페이지 렌더링 후 가장 처음 호출되는 함수
-    useEffect(() => {
-        
-        
-    },
-    // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
-    [])
-
     return(
         <div class="create">
             <h2 class="title">CREATE</h2>
@@ -56,8 +90,8 @@ function Create() {
                     <label class="totalmember" htmlFor='input_totalmember'>인원</label>
                 </div>
                 <div>
-                    <input class="roomname_i" type='text' name='input_roomname' placeholder="방제를 작성해주세요"/>
-                    <input class="totalmember_i" type='number' min="1" max="3" name='input_totalmember' placeholder="모집인원"/>
+                    <input class="roomname_i" type='text' name='input_roomname' placeholder="방제를 작성해주세요" onChange={onRoomnameChange} value={roomname} />
+                    <input class="totalmember_i" type='number' min="1" max="3" name='input_totalmember' placeholder="모집인원" onChange={onTotalmemberChange} value={totalmember} />
                 </div>
             </div>
             <div>
@@ -66,8 +100,8 @@ function Create() {
                     <label class="endpoint" htmlFor='input_endpoint'>도착지</label>
                 </div>
                 <div>
-                    <input class="startpoint_i" type='text' name='input_startpoint' placeholder="자세히 작성해주세요"/>
-                    <input class="endpoint_i" type='text' name='input_endpoint' placeholder="자세히 작성해주세요"/>
+                    <input class="startpoint_i" type='text' name='input_startpoint' placeholder="자세히 작성해주세요" onChange={onStartpointChange} value={startpoint} />
+                    <input class="endpoint_i" type='text' name='input_endpoint' placeholder="자세히 작성해주세요" onChange={onEndpointChange} value={endpoint} />
                 </div>
             </div>
             <div>
@@ -76,7 +110,7 @@ function Create() {
                     <label class="starttime" htmlFor='input_starttime'>출발 시간</label>
                 </div>
                 <div>
-                    <input class="startdate_ia" type='number' min="1" max="12" name='input_startdate' placeholder="월"/>
+                    <input class="startdate_ia" type='number' min="1" max="12" name='input_startdate' placeholder="월" onChange={onStarttimeChange} value={starttime} />
                     <input class="startdate_ib" type='number' min="1" max="31" name='input_startdate' placeholder="일"/>
                     <input class="starttime_ia" type='number' min="1" max="24" name='input_starttime' placeholder="시(24시 기준)"/>
                     <input class="starttime_ib" type='number' min="0" max="59" name='input_starttime' placeholder="분"/>
