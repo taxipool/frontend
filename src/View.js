fbf3d49993e5b95bd668af543743f4a068a4617a
hj-language <idgafff@kumoh.ac.kr>
2022-03-16 21:02:09 +0900

    22.3.16 view에서 데이터 띄우기 성공!!!

--- src/View.js
+++ src/View.js
@@ -3,53 +3,69 @@ import axios from 'axios';
 import SingleComment from './SingleComment'
 import './View.css';
 
-function ride_button_click() {
-    alert('정상적으로 신청되었습니다!');
+/*
+    - 댓글 구현
+    - 수정, 삭제 => 방장만
+    - 타기, 내리기 바뀌는 거
+    - 버튼 클릭했을 때 페이지 이동
+*/
+
+function OnClickRide() {
+    let obj = {
+        params: {
+            isRide: true,
+        }
+    };
+    
+    let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im15aWQiLCJpYXQiOjE1MTYyMzkwMjJ9.SrLa4xS_VbNwYF4Zatu7ilRXCKrOlccvkBPHYV5yJSc"
+    
+    let config = {
+        headers: { Authorization: `Bearer ${accessToken}` }
+    };
+
+    let roomnum = 1;
+
+    axios.put(`http://localhost:1000/api/rooms/${roomnum}`, obj, config)
+    .then((res) => {
+        console.log(res);
+        alert('정상적으로 신청되었습니다!');
+    })
+    .catch((err) => { 
+        console.log(err);
+        alert('신청에 실패했습니다!');
+    })
+    
 }
 
-const View = () => {
-    const [inputs, setInputs] = useState({
-        vRoomname: "",
-        vStartpoint: "",
-        vEndpoint: "",
-        vStarttime: new Date(),
-        vTotalmember: '',
-        vCurrentmember: '',
-        vLeader: ''
-    })
-
-    const onInputChange = (e) => {
-        const { value, key } = e.target;
-        setInputs({
-            ...inputs,
-            [key] : value
-        })
-    }
+function View() {
+    const [roomname, setRoomname] = useState('')
+    const [startpoint, setStartpoint] = useState('')
+    const [endpoint, setEndpoint] = useState('')
+    const [starttime, setStarttime] = useState('')
+    const [totalmember, setTotalmember] = useState('')
+    const [currentmember, setCurrentmember] = useState('')
+    const [isLeader, setIsLeader] = useState('')
+    const [isRide, setIsRide] = useState('')
 
     useEffect(() => {
-        let obj = {
-            roomnum: "01"
-        };
-
         let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im15aWQiLCJpYXQiOjE1MTYyMzkwMjJ9.SrLa4xS_VbNwYF4Zatu7ilRXCKrOlccvkBPHYV5yJSc"
     
         let config = {
             headers: { Authorization: `Bearer ${accessToken}` }
          };
 
-        axios.get(`https://taxipool.iptime.org:8080/api/rooms/${obj.roomnum}`, '', config)
+        axios.get(`http://localhost:1000/api/rooms/${window.location.pathname.slice(6, )}`, '', config)
         .then(res => {
-                console.log(res);
-                inputs.vRoomname = res.roomname;
-                inputs.vStartpoint = res.startpoint;
-                inputs.vEndpoint = res.endpoint;
-                inputs.vStarttime = res.starttime;
-                inputs.vTotalmember = res.totalmember;
-                inputs.vCurrentmember = res.currentmember;
-                inputs.vLeader = res.leaderid; // 닉네임
-
-                // sessionStorage.setItem('token', res.token);
-                // token = sessionStorage.getItem('token');
+            setRoomname(res.data.room.roomname);
+            setStartpoint(res.data.room.startpoint);
+            setEndpoint(res.data.room.endpoint);
+            setStarttime(res.data.room.starttime);
+            setTotalmember(res.data.room.totalmember);
+            setCurrentmember(res.data.room.currentmember);
+            setIsLeader(res.data.room.leaderid);
+            setIsRide(res.data.room.isRide);
+            // sessionStorage.setItem('token', res.token);
+            // token = sessionStorage.getItem('token');
             }
         )
         .catch(err => console.log(err))
@@ -62,17 +78,17 @@ const View = () => {
             <h2 class="title">ROOM</h2>
             <hr></hr>
             <div>
-                <label class="roomname">방제 : {inputs.vRoomname}</label>
+                <label class="roomname">방제 : {roomname}</label>
             </div>
             <div>
                 <label class="roomview">
                     <div class="maintext_1">[방정보]</div>
-                    <div>출 발 지 : {inputs.vStartpoint}</div>
-                    <div>도 착 지 : {inputs.vEndpoint}</div>
-                    <div>출발 날짜 : {inputs.vStarttime}</div>
-                    <div>출발 시간 : </div>
-                    <div>총 인 원 : {inputs.vTotalmember}</div>
-                    <div>현재 인원 : {inputs.vCurrentmember}</div>
+                    <div>출 발 지 : {startpoint}</div>
+                    <div>도 착 지 : {endpoint}</div>
+                    <div>출발 날짜 : {starttime.slice(0,10)}</div>
+                    <div>출발 시간 : {starttime.slice(11,13)+"시 "+starttime.slice(14,16)+"분"}</div>
+                    <div>총 인 원 : {totalmember+"명"}</div>
+                    <div>현재 인원 : {currentmember}</div>
                 </label>
             </div>
             <div>
@@ -83,13 +99,12 @@ const View = () => {
                 <a href="/main"><button class="list" type='button'>
                     목록
                 </button></a>
-                <button class="ride" type='submit' onClick={ride_button_click}>
+                <button class="ride" type='submit' onClick={OnClickRide}>
                     타기
                 </button>
                 <div>
                     {
-                        inputs.vLeader == 1
-                        ? <a href="/Update"><button class="modify" type='button'>
+                        isLeader ? <a href="/Update"><button class="modify" type='button'>
                             수정</button></a>
                         : null
                     }
