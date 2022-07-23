9327c19f8efc845262965dcd93ac7d19c3f87f3e
suhye0n <contact.suhyeon@gmail.com>
2022-07-24 06:29:29 +0900

    220813 메인페이지 통신 성공

--- src/Main.js
+++ src/Main.js
@@ -3,27 +3,50 @@ import axios from 'axios';
 import './Main.css';
 import icon from './icon.png'
 
-function Main() {
-  const [posts, setPosts] = useState([]);
+import CommonTable from './table/CommonTable';
+import CommonTableColumn from './table/CommonTableColumn';
+import CommonTableRow from './table/CommonTableRow';
 
-  const getPosts = () => {
-    // axios.get('http://kittaxipool.iptime.org:3000/api/rooms')
-    axios.get('http://localhost:3001/api/rooms')
-    .then((res) => {
+function GetData() {
+  
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://kittaxipool.iptime.org:3000/api/rooms')
+    .then((res)=> {
       console.log(res.data)
-      setPosts(res.data);
+      setData(res.data);
     })
     .catch(err => console.log(err, '통신 실패!!'))
-  }
+  }, []);
 
-  useEffect(getPosts, []);
+  const item = (Object.values(data)).map((item) => (
+    <CommonTableRow key={item.id}>
+      <CommonTableColumn>{item.leaderid}</CommonTableColumn>
+      <CommonTableColumn>{item.roomname}</CommonTableColumn>
+      <CommonTableColumn>{item.startpoint}</CommonTableColumn>
+      <CommonTableColumn>{item.endpoint}</CommonTableColumn>
+      <CommonTableColumn>{item.starttime}</CommonTableColumn>
+      <CommonTableColumn>{item.currentmember}</CommonTableColumn>
+      <CommonTableColumn>{item.totalmember}</CommonTableColumn>
+      <CommonTableColumn>{item.createtime}</CommonTableColumn>
+    </CommonTableRow>
+  ));
+
+  return item;
+}
+
+function Main() {
+  const item = GetData();
   
   return(
     <div className="main">
       <h2 class="title">LIST</h2>
       <hr></hr>
 
-      <li items={posts}/>
+      <CommonTable headersName={['방장', '제목', '출발지', '도착지', '시각', '인원', '생성일']}>
+        {item}
+      </CommonTable>
 
       <form className="search">
         <p class="search-title">검색</p>
