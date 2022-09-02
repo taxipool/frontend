e12b030a8aed41072ddb94b47594a2f81aff07c6
suhye0n <contact.suhyeon@gmail.com>
2022-09-02 12:54:15 +0900

    220902 아이디 닉넴 중복확인

---
+++ src/Signout.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Signout.css';
+ 
+function Signout() {
+    return(
+        <
+    )
+}
--- src/Signup.js
+++ src/Signup.js
@@ -30,6 +30,66 @@ function Signup() {
     },
     []);
  
+    const onClickIDCheck = (event) => {
+        // 기본 클릭 동작 방지
+        event.preventDefault()
+        
+        let userObj = {
+            id: id,
+            pw: password,
+            name: name,
+            phone: phonenumber,
+            nickname: nickname
+         };
+
+        axios.post("http://kittaxipool.iptime.org:3000/api/user/member", userObj)
+        .then(res => {
+            // 회원가입 실패
+            if (res.status === 400){
+                alert('중복된 아이디입니다.')
+            }
+            // 회원가입 성공
+            else if (res.status === 200) {
+                alert('사용 가능한 아이디입니다.')
+            }
+            console.log(res);
+        })
+        .catch(err => {
+            alert("네트워크가 불안정합니다.");
+            console.log(err);
+        });
+    }
+ 
+    const onClickNicknameCheck = (event) => {
+        // 기본 클릭 동작 방지
+        event.preventDefault()
+        
+        let userObj = {
+            id: id,
+            pw: password,
+            name: name,
+            phone: phonenumber,
+            nickname: nickname
+         };
+
+        axios.post("http://kittaxipool.iptime.org:3000/api/user/member", userObj)
+        .then(res => {
+            // 회원가입 실패
+            if (res.status === 400){
+                alert('중복된 닉네임입니다.')
+            }
+            // 회원가입 성공
+            else if (res.status === 200) {
+                alert('사용 가능한 닉네임입니다.')
+            }
+            console.log(res);
+        })
+        .catch(err => {
+            alert("네트워크가 불안정합니다.");
+            console.log(err);
+        });
+    }
+ 
     const onClickSignup = (event) => {
         // 기본 클릭 동작 방지
         event.preventDefault()
@@ -42,7 +102,6 @@ function Signup() {
             nickname: nickname
          };
          axios.post("http://kittaxipool.iptime.org:3000/api/user/member", userObj)
-        //  axios.post("http://localhost:3001/api/user/member", userObj)
         .then(res => {
             // 회원가입 실패
             if (res.status === 400){
@@ -56,7 +115,6 @@ function Signup() {
                 window.location.href="/login";
             }
             console.log(res);
-            window.location.href="/login";
         })
         .catch(err => {
             alert("네트워크가 불안정합니다.");
@@ -67,32 +125,34 @@ function Signup() {
     // 디자인
     return(
         <div class="signup">
-            <h2 class="title">SIGNUP</h2>
-            <hr></hr>
-            <div>
-                <label htmlFor='id'>아이디</label>
-                <input placeholder="abcde" maxLength="10" class="check_input" type='text' name='id' value={id} onChange={onIdHandler} />
-                <button class="btn">중복 확인</button>
-            </div>
-            <div>
-                <label htmlFor='password'>비밀번호</label>
-                <input placeholder="********" maxLength="20" type='password' name='password' value={password} onChange={onPasswordHandler} />
-            </div>
-            <div>
-                <label htmlFor='name'>이름</label>
-                <input maxLength="6" placeholder="홍길동" class="check_input" type='text' name='name' value={name} onChange={onNameHandler} />
-                <button class="btn">중복 확인</button>
-            </div>
-            <div>
-                <label htmlFor='phonenumber'>전화번호</label>
-                <input placeholder="010-0000-0000" type='text' name='phonenumber' value={phonenumber} onChange={onPhonenumberHandler} />
-            </div>
-            <div>
-                <label htmlFor='nickname'>닉네임</label>
-                <input placeholder="택시풀" type='text' name='nickname' value={nickname} onChange={onNicknameHandler} />
-            </div>
-            <div>
-                <button class="signup_button" type='button' onClick={onClickSignup}>회원가입</button>
+            <div className='signup-container'>
+                <h2 class="title">SIGNUP</h2>
+                <hr></hr>
+                <div>
+                    <label htmlFor='id'>아이디</label>
+                    <input placeholder="abcde" maxLength="10" class="check_input" type='text' name='id' value={id} onChange={onIdHandler} />
+                    <button class="btn" onClick={onClickIDCheck}>중복 확인</button>
+                </div>
+                <div>
+                    <label htmlFor='password'>비밀번호</label>
+                    <input placeholder="********" maxLength="20" type='password' name='password' value={password} onChange={onPasswordHandler} />
+                </div>
+                <div>
+                    <label htmlFor='name'>이름</label>
+                    <input maxLength="6" placeholder="홍길동" class="check_input" type='text' name='name' value={name} onChange={onNameHandler} />
+                </div>
+                <div>
+                    <label htmlFor='phonenumber'>전화번호</label>
+                    <input placeholder="010-0000-0000" type='text' name='phonenumber' value={phonenumber} onChange={onPhonenumberHandler} />
+                </div>
+                <div>
+                    <label htmlFor='nickname'>닉네임</label>
+                    <input placeholder="택시풀" type='text' name='nickname' value={nickname} onChange={onNicknameHandler} />
+                    <button class="btn" onClick={onClickNicknameCheck}>중복 확인</button>
+                </div>
+                <div>
+                    <button class="signup_button" type='button' onClick={onClickSignup}>회원가입</button>
+                </div>
             </div>
         </div>
     )
